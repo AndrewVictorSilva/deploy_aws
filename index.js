0b6017ff00ba82cad0444293ba9f12e0f84b8351
Andrew Silva <avsilva.andrew@gmail.com>
2021-10-27 21:36:39 -0300

    testing update

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require ('express')
 
 const app = express()
 
+app.get('/atualizou', (req,res) => {
+  return res.json({message:"Atualizou!"})
+})
+
 app.get('/', (req, res) => {
   return res.json({message:'Server is Up!'})
 })
